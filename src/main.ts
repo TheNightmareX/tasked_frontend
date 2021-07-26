13d40cafbb6361164d12b9b1ec2b286f83e8de82
TheNightmareX <TheNightmareX@foxmail.com>
2021-07-26 22:25:03 +0800

    feat: init vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import router from "./router";
 import store from "./store";
 import vuetify from "./vuetify";
 
 const app = createApp(App);
 app.use(vuetify);
+app.use(router);
 app.use(store);
 app.mount("#app");
