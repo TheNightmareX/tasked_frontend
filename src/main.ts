f1607c4e0855402e3978e4a8cac234c2bc3606e6
TheNightmareX <TheNightmareX@foxmail.com>
2021-07-26 22:19:12 +0800

    style: deprecate /src/plugins

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import vuetify from "./plugins/vuetify";
 import store from "./store";
+import vuetify from "./vuetify";
 
 const app = createApp(App);
 app.use(vuetify);
