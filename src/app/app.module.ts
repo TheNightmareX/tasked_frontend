a86774cccb9be52fb4a8e81d7614235c41fbe71f
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-02 12:52:49 +0800

    feat: implement basic layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PartitionOutline } from '@ant-design/icons-angular/icons/';
 import { en_US, NZ_I18N } from 'ng-zorro-antd/i18n';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 registerLocaleData(en);
+
+const icons = [PartitionOutline];
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,6 +25,9 @@ registerLocaleData(en);
     BrowserAnimationsModule,
     FormsModule,
     HttpClientModule,
+    NzIconModule.forRoot(icons),
+    NzLayoutModule,
+    NzMenuModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent],
