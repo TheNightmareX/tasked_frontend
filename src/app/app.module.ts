25774b7bf67455b5e5155e217379ad80372201d9
TheNightmareX <TheNightmareX@foxmail.com>
2021-08-05 19:15:02 +0800

    refactor: ensure each component has a directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { HttpInterceptorsModule } from './http-interceptors/http-interceptors.module';
-import { NotFoundComponent } from './not-found.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 import { SharedModule } from './shared/shared.module';
 
 registerLocaleData(en);
