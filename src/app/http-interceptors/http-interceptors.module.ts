a0ef3099828d2fc45f2c88c95bd1dba7ff11b640
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-01 00:00:31 +0800

    refactor: remove RESTful related code

--- src/app/http-interceptors/http-interceptors.module.ts
+++ src/app/http-interceptors/http-interceptors.module.ts
@@ -1,9 +1,6 @@
 import { HttpInterceptor, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule, Provider, Type } from '@angular/core';
 import { AuthInterceptor } from './auth.interceptor';
-import { ListInterceptor } from './list.interceptor';
-import { PrefixInterceptor } from './prefix.interceptor';
-import { TransformerInterceptor } from './transformer.interceptor';
 
 const register = (type: Type<HttpInterceptor>): Provider => ({
   provide: HTTP_INTERCEPTORS,
@@ -12,11 +9,6 @@ const register = (type: Type<HttpInterceptor>): Provider => ({
 });
 
 @NgModule({
-  providers: [
-    register(AuthInterceptor),
-    register(ListInterceptor),
-    register(TransformerInterceptor),
-    register(PrefixInterceptor),
-  ],
+  providers: [register(AuthInterceptor)],
 })
 export class HttpInterceptorsModule {}
