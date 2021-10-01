210acfddc8b519e317bba41b5be3dee47488983e
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-02 00:57:29 +0800

    feat: implement the signup page

--- src/app/auth/auth-form-signup/auth-form-signup.component.ts
+++ src/app/auth/auth-form-signup/auth-form-signup.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
 import { forkJoin, of, Subject, timer } from 'rxjs';
 import { catchError, concatMap, map, throttleTime } from 'rxjs/operators';
 import { AuthService } from 'src/app/core/auth.service';
-import { UserCreateInput, Gender, CreateUserGQL } from 'src/app/graphql';
+import { CreateUserGQL, Gender, UserCreateInput } from 'src/app/graphql';
+import { NotificationType } from 'src/app/notification-type.enum';
 
 @Component({
   selector: 'app-auth-form-signup',
@@ -25,13 +27,13 @@ export class AuthFormSignupComponent implements OnInit {
     gender: Gender.Unknown,
   };
 
-  isLoading = false;
+  loading = false;
 
   submit$ = new Subject<Event>();
 
   constructor(
     private auth: AuthService,
-    private messenger: NzMessageService,
+    private notifier: NotifierService,
     private router: Router,
     private createUserSql: CreateUserGQL,
   ) {}
@@ -41,7 +43,7 @@ export class AuthFormSignupComponent implements OnInit {
   }
 
   private submit() {
-    this.isLoading = true;
+    this.loading = true;
     const { username, password, nickname, gender } = this.data;
     const data: UserCreateInput = { username, password, nickname, gender };
     forkJoin([
@@ -55,8 +57,12 @@ export class AuthFormSignupComponent implements OnInit {
       .pipe(map((values) => values[0]))
       .subscribe((user) => {
         if (user) this.router.navigate(['/']);
-        else this.messenger.error('Username is already taken');
-        this.isLoading = false;
+        else
+          this.notifier.notify(
+            NotificationType.Error,
+            'Username is already taken',
+          );
+        this.loading = false;
       });
   }
 }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -13,6 +13,7 @@ import { AuthComponent } from './auth.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatFormFieldModule,
     MatInputModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
   ],
 })
 export class AuthModule {}
