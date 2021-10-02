242d9fe14b6ac4336a82a3a335d7d0351d0eac7c
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-02 22:01:29 +0800

    feat: implement updating active classroom state

--- src/app/classrooms/classroom-detail/classroom-detail.component.ts
+++ src/app/classrooms/classroom-detail/classroom-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { ClassroomsStateService } from 'src/app/core/classrooms-state.service';
 
 @Component({
   selector: 'app-classroom-detail',
@@ -6,7 +9,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classroom-detail.component.css'],
 })
 export class ClassroomDetailComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private route: ActivatedRoute,
+    private state: ClassroomsStateService,
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.paramMap
+      .pipe(
+        map((params) => params.get('id')),
+        filter((value): value is string => !!value),
+        map((value) => +value),
+      )
+      .subscribe((classroomId) => {
+        this.state.activeId = classroomId;
+      });
+  }
 }
