6e2f9737e466d8b4b9651e782a475f3f4cc0b9b1
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-03 23:28:54 +0800

    feat: implement classroom membership list

--- src/app/classrooms/classroom-membership-list/classroom-membership-list.component.ts
+++ src/app/classrooms/classroom-membership-list/classroom-membership-list.component.ts
@@ -1,4 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ClassroomsStateService } from 'src/app/core/classrooms-state.service';
+import {
+  ClassroomMembershipListGQL,
+  ClassroomMembershipListQuery,
+  Gender,
+  Role,
+} from 'src/app/graphql';
+
+type Membership =
+  ClassroomMembershipListQuery['classroom']['memberships']['results'][number];
+
+type Classroom = ClassroomMembershipListQuery['classroom'];
 
 @Component({
   selector: 'app-classroom-membership-list',
@@ -6,7 +20,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classroom-membership-list.component.css'],
 })
 export class ClassroomMembershipListComponent implements OnInit {
-  constructor() {}
+  classroom$: Observable<Classroom> = of();
+  memberships$: Observable<Membership[]> = of();
+  Role = Role;
+  Gender = Gender;
 
-  ngOnInit() {}
+  constructor(
+    private state: ClassroomsStateService,
+    private classroomMembershipListGql: ClassroomMembershipListGQL,
+  ) {}
+
+  ngOnInit() {
+    this.classroom$ = this.classroomMembershipListGql
+      .fetch({ id: this.state.activeId! + '' })
+      .pipe(map(({ data }) => data.classroom));
+    this.memberships$ = this.classroom$.pipe(
+      map((classroom) => classroom.memberships.results),
+      map((memberships) => [...memberships].sort(this.membershipComparer)),
+    );
+  }
+
+  getDisplayName(membership: Membership) {
+    return (
+      membership.displayName ??
+      membership.owner.nickname ??
+      membership.owner.username
+    );
+  }
+
+  private membershipComparer = (
+    membershipA: Membership,
+    membershipB: Membership,
+  ) =>
+    membershipA.role == membershipB.role
+      ? 0
+      : membershipA.role == Role.Teacher
+      ? -1
+      : 1;
 }
