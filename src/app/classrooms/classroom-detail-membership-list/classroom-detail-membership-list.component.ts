f04cc23c6a55210b188c046d1ab2e45fe5f23a84
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-23 22:23:18 +0800

    fix(memberships): ensure the creator to be at the top of the list

--- src/app/classrooms/classroom-detail-membership-list/classroom-detail-membership-list.component.ts
+++ src/app/classrooms/classroom-detail-membership-list/classroom-detail-membership-list.component.ts
@@ -4,6 +4,7 @@ import { combineLatest, Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from 'src/app/auth/auth.service';
 import {
+  ClassroomDetailGQL,
   ClassroomMembershipListGQL,
   ClassroomMembershipListQuery,
   Role,
@@ -23,26 +24,39 @@ export class ClassroomDetailMembershipListComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private auth: AuthService,
-    private classroomMembershipListGql: ClassroomMembershipListGQL,
+    private listGql: ClassroomMembershipListGQL,
+    private classroomGql: ClassroomDetailGQL,
   ) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
+      const id = params.get('id')!;
       this.memberships$ = combineLatest([
-        this.classroomMembershipListGql
-          .watch({ id: params.get('id')! })
+        this.listGql
+          .watch({ id })
           .valueChanges.pipe(
             map((result) => [...result.data.classroom.memberships.results]),
           ),
+        this.classroomGql
+          .watch({ id })
+          .valueChanges.pipe(map((result) => result.data.classroom)),
         this.auth.user$,
       ]).pipe(
-        map(([memberships, user]) =>
+        map(([memberships, classroom, user]) =>
           memberships
             .sort((a, b) =>
               a.owner.id == user!.id ? -1 : b.owner.id == user!.id ? 1 : 0,
             )
             .sort((a, b) =>
-              a.role == b.role ? 0 : a.role == Role.Teacher ? -1 : 1,
+              a.owner.id == classroom.creator.id
+                ? -1
+                : b.owner.id == classroom.creator.id
+                ? 1
+                : a.role == b.role
+                ? 0
+                : a.role == Role.Teacher
+                ? -1
+                : 1,
             ),
         ),
       );
