835438d80e530aa9af2033b36cc4483995bff2b8
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-23 22:29:27 +0800

    fix: import the missing `MatTooltipModule`

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,16 +2,21 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
@@ -19,14 +24,9 @@ import { EqualDirective } from './equal.directive';
 import { FormProfileComponent } from './form-profile/form-profile.component';
 import { LayoutComponent } from './layout/layout.component';
 import { LoadingDirective } from './loading.directive';
-import { ProfileBtnComponent } from './profile-btn/profile-btn.component';
-import { ProfileBtnMenuComponent } from './profile-btn-menu/profile-btn-menu.component';
 import { ProfileBtnMenuDialogEditComponent } from './profile-btn-menu-dialog-edit/profile-btn-menu-dialog-edit.component';
-import { MatListModule } from '@angular/material/list';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { MatRippleModule } from '@angular/material/core';
-import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { ProfileBtnMenuComponent } from './profile-btn-menu/profile-btn-menu.component';
+import { ProfileBtnComponent } from './profile-btn/profile-btn.component';
 
 const modules = [
   CommonModule,
@@ -50,6 +50,7 @@ const modules = [
     MatProgressSpinnerModule,
     MatCardModule,
     MatBottomSheetModule,
+    MatTooltipModule,
   ],
 ];
 const components = [
