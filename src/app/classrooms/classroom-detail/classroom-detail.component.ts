5a465c083088d7f520962cf018afca717e49b81b
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-29 20:54:03 +0800

    feat(classrooms/detail): navigate to the assignments page when as student

--- src/app/classrooms/classroom-detail/classroom-detail.component.ts
+++ src/app/classrooms/classroom-detail/classroom-detail.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { BreakpointsService } from 'src/app/core/breakpoints.service';
-import { ClassroomDetailGQL, ClassroomDetailQuery } from 'src/app/graphql';
+import {
+  ClassroomDetailGQL,
+  ClassroomDetailQuery,
+  Role,
+} from 'src/app/graphql';
 import { ClassroomsLocalStorageService } from '../classrooms-local-storage.service';
 
 type Classroom = ClassroomDetailQuery['classroom'];
@@ -13,14 +17,17 @@ type Classroom = ClassroomDetailQuery['classroom'];
   templateUrl: './classroom-detail.component.html',
   styleUrls: ['./classroom-detail.component.css'],
 })
-export class ClassroomDetailComponent implements OnInit {
+export class ClassroomDetailComponent implements OnInit, OnDestroy {
   classroom$!: Observable<Classroom>;
 
   links: Link[] = [['Assignments', ['assignments']]];
-  linksActive?: Link;
+  linkActive?: Link;
+
+  private sub!: Subscription;
 
   constructor(
     public breakpoints: BreakpointsService,
+    private router: Router,
     private route: ActivatedRoute,
     private local: ClassroomsLocalStorageService,
     private classroomDetailGql: ClassroomDetailGQL,
@@ -29,11 +36,27 @@ export class ClassroomDetailComponent implements OnInit {
   ngOnInit() {
     this.route.paramMap.subscribe((params) => {
       const id = params.get('id')!;
+
       this.local.lastActiveId = id;
       this.classroom$ = this.classroomDetailGql
         .watch({ id })
         .valueChanges.pipe(map(({ data }) => data.classroom));
+
+      this.sub = this.classroom$.subscribe((classroom) => {
+        if (classroom.membership.role == Role.Student)
+          this.navigate(this.links[0]);
+        // TODO: navigate to a assignments management page when the role is teacher
+      });
     });
+  }
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
+
+  navigate(link: Link) {
+    this.router.navigate(link[1], { relativeTo: this.route });
+    this.linkActive = link;
   }
 }
 
