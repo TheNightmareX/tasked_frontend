892e586f3fdba9d1e304dd8f2aae2cd4b514f715
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-29 20:23:05 +0800

    feat(classrooms/redirection): check classroom accessibility  before redirection

--- src/app/classrooms/classroom-redirector/classroom-redirector.component.ts
+++ src/app/classrooms/classroom-redirector/classroom-redirector.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+import { ClassroomListGQL } from 'src/app/graphql';
 import { ClassroomsLocalStorageService } from '../classrooms-local-storage.service';
 
 @Component({
@@ -12,12 +14,28 @@ export class ClassroomRedirectorComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private storage: ClassroomsLocalStorageService,
+    private listGql: ClassroomListGQL,
   ) {}
 
   ngOnInit() {
     if (this.storage.lastActiveId)
-      this.router.navigate(['../', this.storage.lastActiveId], {
-        relativeTo: this.route,
-      });
+      this.listGql
+        .fetch()
+        .pipe(map((result) => result.data.classrooms.results))
+        .subscribe((classrooms) => {
+          const existsClassroom = classrooms.some(
+            (item) => item.id == this.storage.lastActiveId,
+          );
+          if (!existsClassroom) this.storage.lastActiveId = undefined;
+        });
+
+    this.redirect();
+  }
+
+  private redirect() {
+    const id = this.storage.lastActiveId;
+    this.router.navigate(['../', ...(id ? [id] : [])], {
+      relativeTo: this.route,
+    });
   }
 }
