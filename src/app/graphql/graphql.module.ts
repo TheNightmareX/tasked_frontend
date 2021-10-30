f86f3dd865936d4a641c58bf11f06acafb94c6e6
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-30 14:53:34 +0800

    feat(graphql): use `HttpBatchLink`

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -2,18 +2,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { InMemoryCache } from '@apollo/client/core';
 import { APOLLO_OPTIONS } from 'apollo-angular';
-import { HttpLink } from 'apollo-angular/http';
+import { HttpBatchLink } from 'apollo-angular/http';
 
 @NgModule({
   imports: [HttpClientModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
-      useFactory: (httpLink: HttpLink) => ({
-        link: httpLink.create({ uri: '/graphql/' }),
+      useFactory: (link: HttpBatchLink) => ({
+        link: link.create({ uri: '/graphql/' }),
         cache: new InMemoryCache(),
       }),
-      deps: [HttpLink],
+      deps: [HttpBatchLink],
     },
   ],
 })
