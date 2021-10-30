317ab5d7aed94b349ce62fd5262f40554aaf2f70
TheNightmareX <TheNightmareX@foxmail.com>
2021-10-30 16:14:43 +0800

    fix(shared/popup): directly open the template instead of itself as the popup.
    Now there won't be an `app-popup` wrapping the popup content so the
    styles can work better.

--- src/app/shared/popup/popup.component.ts
+++ src/app/shared/popup/popup.component.ts
@@ -1,29 +1,15 @@
-import {
-  Component,
-  Inject,
-  OnInit,
-  Optional,
-  TemplateRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import {
   MatBottomSheet,
   MatBottomSheetRef,
-  MAT_BOTTOM_SHEET_DATA,
 } from '@angular/material/bottom-sheet';
-import {
-  MatDialog,
-  MatDialogRef,
-  MAT_DIALOG_DATA,
-} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { take } from 'rxjs/operators';
 import { BreakpointsService } from 'src/app/core/breakpoints.service';
 
 /**
- * A wrapper of {@link MatDialog} and {@link MatBottomSheet}, which displays a
- * dialog in desktop devices and a bottom sheet in mobile devices.
- *
- * This component is both the popup opener and the popup content.
+ * A wrapper of {@link MatDialog} and {@link MatBottomSheet}, which opens its
+ * content as a dialog in desktop devices but a bottom sheet in mobile devices.
  */
 @Component({
   selector: 'app-popup',
@@ -32,7 +18,7 @@ import { BreakpointsService } from 'src/app/core/breakpoints.service';
 })
 export class PopupComponent implements OnInit {
   @ViewChild(TemplateRef)
-  private contentTemplate?: TemplateRef<never>;
+  private contentTemplate!: TemplateRef<never>;
 
   private sheetRef?: MatBottomSheetRef<PopupComponent>;
   private dialogRef?: MatDialogRef<PopupComponent>;
@@ -41,37 +27,19 @@ export class PopupComponent implements OnInit {
     private breakpoints: BreakpointsService,
     private dialog: MatDialog,
     private sheet: MatBottomSheet,
-
-    @Optional()
-    @Inject(MAT_DIALOG_DATA)
-    private contentTemplateWhenAsDialog?: TemplateRef<never>,
-
-    @Optional()
-    @Inject(MAT_BOTTOM_SHEET_DATA)
-    private contentTemplateWhenAsSheet?: TemplateRef<never>,
   ) {}
 
   ngOnInit() {}
 
   open() {
     this.breakpoints.phone$.pipe(take(1)).subscribe((isPhone) => {
-      if (isPhone) {
-        this.sheetRef = this.sheet.open(PopupComponent, {
-          data: this.contentTemplate,
-        });
-      } else
-        this.dialogRef = this.dialog.open(PopupComponent, {
-          data: this.contentTemplate,
-        });
+      if (isPhone) this.sheetRef = this.sheet.open(this.contentTemplate);
+      else this.dialogRef = this.dialog.open(this.contentTemplate);
     });
   }
 
   close() {
     this.dialogRef?.close();
     this.sheetRef?.dismiss();
-  }
-
-  get contentTemplateWhenAsPopup() {
-    return this.contentTemplateWhenAsDialog || this.contentTemplateWhenAsSheet;
   }
 }
