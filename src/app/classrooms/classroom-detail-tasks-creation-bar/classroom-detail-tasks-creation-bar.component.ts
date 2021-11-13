e12efbcd21e792ecee7e78102ee61b0527a5eecb
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-13 18:12:46 +0800

    feat(classrooms/tasks/creation): implement

---
+++ src/app/classrooms/classroom-detail-tasks-creation-bar/classroom-detail-tasks-creation-bar.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NotifierService } from 'angular-notifier';
+import { finalize } from 'rxjs/operators';
+import { leastTime } from 'src/app/common/least-time.operator';
+import { NotificationType } from 'src/app/common/notification-type.enum';
+import { ApolloHelperService } from 'src/app/core/apollo-helper.service';
+import {
+  ClassroomTaskListGQL,
+  ClassroomTaskListQuery,
+  ClassroomTaskListQueryVariables,
+  TaskCreateGQL,
+} from 'src/app/graphql';
+
+@Component({
+  selector: 'app-classroom-detail-tasks-creation-bar',
+  templateUrl: './classroom-detail-tasks-creation-bar.component.html',
+  styleUrls: ['./classroom-detail-tasks-creation-bar.component.css'],
+})
+export class ClassroomDetailTasksCreationBarComponent implements OnInit {
+  data = '';
+  loading = false;
+
+  constructor(
+    private route: ActivatedRoute,
+    private notifier: NotifierService,
+    private createGql: TaskCreateGQL,
+    private classroomTaskListGql: ClassroomTaskListGQL,
+    private apolloHelper: ApolloHelperService,
+  ) {}
+
+  ngOnInit() {}
+
+  create() {
+    if (this.loading) return;
+    const classroomId = this.route.parent!.snapshot.paramMap.get('id')!;
+    this.loading = true;
+    this.createGql
+      .mutate(
+        { data: { classroom: classroomId, title: this.data } },
+        {
+          update: (_, result) => {
+            this.apolloHelper.updateQueryCache<
+              ClassroomTaskListQuery,
+              ClassroomTaskListQueryVariables
+            >({
+              query: this.classroomTaskListGql.document,
+              data: (prev) => ({
+                ...prev,
+                classroom: {
+                  ...prev.classroom,
+                  tasks: {
+                    ...prev.classroom.tasks,
+                    results: [
+                      result.data!.createTask,
+                      ...prev.classroom.tasks.results,
+                    ],
+                  },
+                },
+              }),
+              variables: { id: classroomId },
+            });
+          },
+        },
+      )
+      .pipe(
+        leastTime(1000),
+        finalize(() => {
+          this.loading = false;
+        }),
+      )
+      .subscribe(
+        () => {
+          this.notifier.notify(NotificationType.Success, 'Task created');
+          this.data = '';
+        },
+        () => {
+          this.notifier.notify(
+            NotificationType.Error,
+            'Failed to create the task',
+          );
+        },
+      );
+  }
+}
--- src/app/classrooms/classrooms.module.ts
+++ src/app/classrooms/classrooms.module.ts
@@ -4,20 +4,21 @@ import { ClassroomCreationComponent } from './classroom-creation/classroom-creat
 import { ClassroomDetailAssignmentsItemDetailComponent } from './classroom-detail-assignments-item-detail/classroom-detail-assignments-item-detail.component';
 import { ClassroomDetailAssignmentsItemComponent } from './classroom-detail-assignments-item/classroom-detail-assignments-item.component';
 import { ClassroomDetailAssignmentsComponent } from './classroom-detail-assignments/classroom-detail-assignments.component';
+import { ClassroomDetailSettingsActionsComponent } from './classroom-detail-settings-actions/classroom-detail-settings-actions.component';
 import { ClassroomDetailSettingsSectionComponent } from './classroom-detail-settings-section/classroom-detail-settings-section.component';
 import { ClassroomDetailSettingsComponent } from './classroom-detail-settings/classroom-detail-settings.component';
 import { ClassroomDetailSidebarMembershipListItemMenuComponent } from './classroom-detail-sidebar-membership-list-item-menu/classroom-detail-sidebar-membership-list-item-menu.component';
 import { ClassroomDetailSidebarMembershipListItemComponent } from './classroom-detail-sidebar-membership-list-item/classroom-detail-sidebar-membership-list-item.component';
 import { ClassroomDetailSidebarMembershipListComponent } from './classroom-detail-sidebar-membership-list/classroom-detail-sidebar-membership-list.component';
 import { ClassroomDetailSidebarComponent } from './classroom-detail-sidebar/classroom-detail-sidebar.component';
+import { ClassroomDetailTabRedirectorComponent } from './classroom-detail-tab-redirector/classroom-detail-tab-redirector.component';
+import { ClassroomDetailTasksCreationBarComponent } from './classroom-detail-tasks-creation-bar/classroom-detail-tasks-creation-bar.component';
+import { ClassroomDetailTasksItemComponent } from './classroom-detail-tasks-item/classroom-detail-tasks-item.component';
+import { ClassroomDetailTasksComponent } from './classroom-detail-tasks/classroom-detail-tasks.component';
 import { ClassroomDetailComponent } from './classroom-detail/classroom-detail.component';
 import { ClassroomRedirectorComponent } from './classroom-redirector/classroom-redirector.component';
 import { ClassroomsRoutingModule } from './classrooms-routing.module';
 import { ClassroomsComponent } from './classrooms.component';
-import { ClassroomDetailSettingsActionsComponent } from './classroom-detail-settings-actions/classroom-detail-settings-actions.component';
-import { ClassroomDetailTabRedirectorComponent } from './classroom-detail-tab-redirector/classroom-detail-tab-redirector.component';
-import { ClassroomDetailTasksComponent } from './classroom-detail-tasks/classroom-detail-tasks.component';
-import { ClassroomDetailTasksItemComponent } from './classroom-detail-tasks-item/classroom-detail-tasks-item.component';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { ClassroomDetailTasksItemComponent } from './classroom-detail-tasks-item
     ClassroomDetailAssignmentsItemDetailComponent,
     ClassroomDetailTasksComponent,
     ClassroomDetailTasksItemComponent,
+    ClassroomDetailTasksCreationBarComponent,
     ClassroomDetailSettingsComponent,
     ClassroomDetailSettingsSectionComponent,
     ClassroomDetailSettingsActionsComponent,
