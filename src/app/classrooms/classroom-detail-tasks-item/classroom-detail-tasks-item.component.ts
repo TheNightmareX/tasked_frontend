c1e7e82ae901ec9edafedb5252a3a92f69750d52
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-26 18:43:53 +0800

    feat(classrooms/tasks/edit): check modification

--- src/app/classrooms/classroom-detail-tasks-item/classroom-detail-tasks-item.component.ts
+++ src/app/classrooms/classroom-detail-tasks-item/classroom-detail-tasks-item.component.ts
@@ -1,9 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { NotifierService } from 'angular-notifier';
-import { Observable } from 'rxjs';
-import { finalize } from 'rxjs/operators';
 import { filterKeys } from 'src/app/common/filter-keys.func';
+import { Observable, Subscription } from 'rxjs';
+import { delay, finalize } from 'rxjs/operators';
 import { NotificationType } from 'src/app/common/notification-type.enum';
 import { pick } from 'src/app/common/pick.func';
 import {
@@ -13,6 +21,7 @@ import {
   TaskUpdateGQL,
   TaskUpdateInput,
 } from 'src/app/graphql';
+import { isEmpty } from 'src/app/common/is-empty.func';
 
 type Task = ClassroomTaskListQuery['classroom']['tasks']['results'][number];
 
@@ -21,12 +30,27 @@ type Task = ClassroomTaskListQuery['classroom']['tasks']['results'][number];
   templateUrl: './classroom-detail-tasks-item.component.html',
   styleUrls: ['./classroom-detail-tasks-item.component.css'],
 })
-export class ClassroomDetailTasksItemComponent implements OnInit {
-  @Input() task?: Task;
+export class ClassroomDetailTasksItemComponent
+  implements OnInit, OnDestroy, AfterViewInit
+{
   data: TaskUpdateInput = {};
-
   loading = false;
   expanded = false;
+  modification: TaskUpdateInput = {};
+  modified = false;
+
+  @ViewChild(NgForm) private form!: NgForm;
+  private subscription = new Subscription();
+
+  @Input()
+  get task() {
+    return this._task;
+  }
+  set task(v: Task | undefined) {
+    this._task = v;
+    this.updateModification();
+  }
+  private _task?: Task;
 
   constructor(
     private route: ActivatedRoute,
@@ -37,6 +61,16 @@ export class ClassroomDetailTasksItemComponent implements OnInit {
   ) {}
 
   ngOnInit() {}
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+  ngAfterViewInit() {
+    this.subscription.add(
+      this.form
+        .valueChanges!.pipe(delay(0))
+        .subscribe(() => this.updateModification()),
+    );
+  }
 
   initData() {
     if (this.task) this.data = pick(this.task, ['title', 'description']);
@@ -91,5 +125,15 @@ export class ClassroomDetailTasksItemComponent implements OnInit {
       () => this.notifier.notify(NotificationType.Success, messageOnSucceed),
       () => this.notifier.notify(NotificationType.Error, messageOnFail),
     );
+  }
+
+  private updateModification() {
+    const task = this.task;
+    if (!task) return;
+    this.modification = filterKeys(
+      this.data,
+      (v, k) => v != task[k as keyof Task],
+    );
+    this.modified = isEmpty(this.modification);
   }
 }
