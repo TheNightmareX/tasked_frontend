2cc5f45449b8150fdd7cd5a9cc41ae3fe2bf2b89
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-28 00:51:08 +0800

    feat(common/storage): expose validation result

--- src/app/common/local-storage-item.class.ts
+++ src/app/common/local-storage-item.class.ts
@@ -1,24 +1,25 @@
 export class LocalStorageItem<Value> {
   value: Value;
+  passed = false;
 
   constructor(
     public key: Key,
     private validator: (dirty: unknown) => boolean,
     private initial: Value,
   ) {
-    this.value = this.load();
+    [this.value, this.passed] = this.load();
   }
 
-  load() {
+  load(): [value: Value, passed: boolean] {
     try {
       const raw = localStorage.getItem(this.key);
       if (raw == null) throw new UseInitialValue();
       const dirty = JSON.parse(raw);
       if (!this.validator(dirty)) throw new UseInitialValue();
-      return dirty;
+      return [dirty, true];
     } catch (error) {
       if (error instanceof UseInitialValue || error instanceof SyntaxError)
-        return this.initial;
+        return [this.initial, false];
       else throw error;
     }
   }
