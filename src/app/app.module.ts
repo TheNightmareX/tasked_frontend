e41ccd71301b979d16d1a8d023dac6796c1858d1
TheNightmareX <TheNightmareX@foxmail.com>
2021-11-27 13:49:56 +0800

    refactor(app): remove the shared module from imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,14 @@ import en from '@angular/common/locales/en';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 import { NotifierModule } from 'angular-notifier';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { GraphqlModule } from './graphql/graphql.module';
 import { LayoutsModule } from './layouts/layouts.module';
-import { SharedModule } from './shared/shared.module';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
 
 registerLocaleData(en);
 
@@ -29,7 +28,6 @@ registerLocaleData(en);
     NotifierModule,
     GraphqlModule,
     CoreModule,
-    SharedModule,
     LayoutsModule,
     AppRoutingModule,
   ],
