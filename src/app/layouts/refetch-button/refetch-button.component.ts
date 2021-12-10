979baf6e2ab1094579d0eacdce3cbabbd2e2aa5c
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-10 20:55:57 +0800

    feat(layouts/refetch-button): notify on complete

--- src/app/layouts/refetch-button/refetch-button.component.ts
+++ src/app/layouts/refetch-button/refetch-button.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { NotifierService } from 'angular-notifier';
 import { Apollo } from 'apollo-angular';
 import { from } from 'rxjs';
 import { finalize, tap } from 'rxjs/operators';
 import { leastTime } from 'src/app/common/least-time.operator';
+import { NotificationType } from 'src/app/common/notification-type.enum';
 
 @Component({
   selector: 'app-refetch-button',
@@ -13,7 +15,7 @@ export class RefetchButtonComponent implements OnInit {
   loading = false;
   disabled = false;
 
-  constructor(private apollo: Apollo) {}
+  constructor(private apollo: Apollo, private notifier: NotifierService) {}
 
   ngOnInit() {}
 
@@ -24,7 +26,13 @@ export class RefetchButtonComponent implements OnInit {
     from(this.apollo.client.refetchQueries({ include: 'active' }))
       .pipe(
         leastTime(1000),
-        tap(() => (this.loading = false)),
+        tap(() => {
+          this.loading = false;
+          this.notifier.notify(
+            NotificationType.Success,
+            $localize`Data refreshed`,
+          );
+        }),
         leastTime(3000),
         tap(() => (this.disabled = false)),
         finalize(() => {
