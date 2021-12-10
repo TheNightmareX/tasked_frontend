ab292e6df533ccafd9112f0d41bd5774da9a2214
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-11 00:34:41 +0800

    refactor: remove unnecessary module imports

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,8 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, SharedModule],
+  imports: [],
   exports: [],
 })
 export class CoreModule {}
--- src/app/validators/validators.module.ts
+++ src/app/validators/validators.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { EqualDirective } from './equal.directive';
 
 @NgModule({
   declarations: [EqualDirective],
-  imports: [CommonModule],
+  imports: [],
   exports: [EqualDirective],
 })
 export class ValidatorsModule {}
