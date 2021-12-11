ba48e49140935db980e4f7bacc54be46176b6d57
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-11 22:12:47 +0800

    refactor(shared/material): merge back to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,25 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatRippleModule } from '@angular/material/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { RouterModule } from '@angular/router';
 import { IntersectionObserverModule } from '@ng-web-apis/intersection-observer';
 import { ValidationModule } from '../validation/validation.module';
@@ -9,7 +28,6 @@ import { AutoFormFieldColorDirective } from './auto-form-field-color.directive';
 import { FetchMoreTriggerComponent } from './fetch-more-trigger/fetch-more-trigger.component';
 import { LoadingDirective } from './loading.directive';
 import { PopupComponent } from './popup/popup.component';
-import { SharedMaterialModule } from './shared-material.module';
 import { UsernameComponent } from './username/username.component';
 
 const modules = [
@@ -18,8 +36,29 @@ const modules = [
   RouterModule,
   FlexLayoutModule,
   IntersectionObserverModule,
+  [
+    MatRippleModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatTabsModule,
+    MatMenuModule,
+    MatDividerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatCardModule,
+    MatBottomSheetModule,
+    MatTooltipModule,
+    MatSlideToggleModule,
+    MatExpansionModule,
+  ],
   ValidationModule,
-  SharedMaterialModule,
 ];
 
 const components = [
