7b4b33c2114040945b450697cc202873f6e83d35
TheNightmareX <TheNightmareX@foxmail.com>
2021-12-17 23:11:00 +0800

    fix(rooms/detail/...): avoid `query.getCurrentResult()`
    It will fail when the target query has not been loaded

--- src/app/rooms/room-detail-assignments/room-detail-assignments.component.ts
+++ src/app/rooms/room-detail-assignments/room-detail-assignments.component.ts
@@ -19,13 +19,13 @@ type Assignment =
   styleUrls: ['./room-detail-assignments.component.scss'],
 })
 export class RoomDetailAssignmentsComponent implements OnInit {
-  assignmentsPending$!: Observable<Assignment[]>;
-  assignmentsCompleted$!: Observable<Assignment[]>;
+  assignmentsPending$?: Observable<Assignment[]>;
+  assignmentsCompleted$?: Observable<Assignment[]>;
   loadingInitial = true;
   loadingMore = false;
   loadingMoreNeeded = false;
 
-  private query!: QueryRef<
+  private query?: QueryRef<
     MembershipAssignmentListQuery,
     MembershipAssignmentListQueryVariables
   >;
@@ -37,55 +37,58 @@ export class RoomDetailAssignmentsComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    const classroomId = this.route.parent!.snapshot.paramMap.get('id')!;
+    const id = this.route.parent!.snapshot.paramMap.get('id')!;
+    this.roomGql
+      .fetch({ id })
+      .pipe(map((result) => result.data.room.membership?.id))
+      .subscribe((id) => {
+        if (!id) return;
 
-    const membershipId = this.roomGql
-      .watch({ id: classroomId })
-      .getCurrentResult().data.room.membership!.id;
+        this.query = this.listGql.watch({ id });
 
-    this.query = this.listGql.watch({ id: membershipId });
-
-    const assignments$ = this.query.valueChanges.pipe(
-      map((result) => result.data.membership.assignments),
-      tap(({ results, total }) => {
-        this.loadingInitial = false;
-        this.loadingMoreNeeded = results.length < total;
-      }),
-      map(({ results }) =>
-        [...results]
-          .sort(
-            (a, b) =>
-              -(
-                new Date(a.updatedAt).getTime() -
-                new Date(b.updatedAt).getTime()
+        const assignments$ = this.query.valueChanges.pipe(
+          map((result) => result.data.membership.assignments),
+          tap(({ results, total }) => {
+            this.loadingInitial = false;
+            this.loadingMoreNeeded = results.length < total;
+          }),
+          map(({ results }) =>
+            [...results]
+              .sort(
+                (a, b) =>
+                  -(
+                    new Date(a.updatedAt).getTime() -
+                    new Date(b.updatedAt).getTime()
+                  ),
+              )
+              .sort((a, b) =>
+                a.isImportant == b.isImportant ? 0 : a.isImportant ? -1 : 1,
               ),
-          )
-          .sort((a, b) =>
-            a.isImportant == b.isImportant ? 0 : a.isImportant ? -1 : 1,
           ),
-      ),
-    );
-    this.assignmentsPending$ = assignments$.pipe(
-      map((items) => items.filter((item) => !item.isCompleted)),
-    );
-    this.assignmentsCompleted$ = assignments$.pipe(
-      map((items) => items.filter((item) => item.isCompleted)),
-    );
+        );
+        this.assignmentsPending$ = assignments$.pipe(
+          map((items) => items.filter((item) => !item.isCompleted)),
+        );
+        this.assignmentsCompleted$ = assignments$.pipe(
+          map((items) => items.filter((item) => item.isCompleted)),
+        );
+      });
   }
 
   fetchMore() {
-    if (!this.loadingMoreNeeded || this.loadingMore) return;
+    const query = this.query;
+    if (!query || !this.loadingMoreNeeded || this.loadingMore) return;
 
-    const data = this.query.getCurrentResult().data.membership.assignments;
+    const data = query.getCurrentResult().data.membership.assignments;
 
     this.loadingMore = true;
-    from(this.query.fetchMore({ variables: { offset: data.results.length } }))
+    from(query.fetchMore({ variables: { offset: data.results.length } }))
       .pipe(
         map((result) => result.data.membership.assignments),
         finalize(() => (this.loadingMore = false)),
       )
       .subscribe(({ results, total }) => {
-        this.query.updateQuery((prev) => ({
+        query.updateQuery((prev) => ({
           ...prev,
           membership: {
             ...prev.membership,
