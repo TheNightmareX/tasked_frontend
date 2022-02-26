9eac7075983fdad27c3a7a3019ea2df0d9312e66
Char2s <Char2s@outlook.com>
2022-02-26 16:30:53 +0800

    refactor(local-storage/base): adjust implementation

--- src/app/local-storage/local-storage-item.class.ts
+++ src/app/local-storage/local-storage-item.class.ts
@@ -1,25 +1,14 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-import { filter } from 'rxjs/operators';
-
-const INITIAL = Symbol();
+import { BehaviorSubject } from 'rxjs';
 
 export class LocalStorageItem<Value> {
-  get value() {
-    return this._value;
-  }
-  private _value!: Value;
-
-  value$: Observable<Value>;
-  private _value$ = new BehaviorSubject<Value | typeof INITIAL>(INITIAL);
+  value!: Value;
+  value$!: BehaviorSubject<Value>;
 
   constructor(
-    public key: Key,
+    public key: string,
     private validator: (dirty: unknown) => boolean,
     private initial: Value | (() => Value),
   ) {
-    this.value$ = this._value$
-      .asObservable()
-      .pipe(filter((value): value is Value => value != INITIAL));
     this.load().save();
   }
 
@@ -28,9 +17,10 @@ export class LocalStorageItem<Value> {
    * @param value
    * @returns
    */
-  next<Next extends Value>(value: Next) {
-    this._value = value;
-    this._value$.next(value);
+  next<Next extends Value>(value: Next): LocalStorageItem<Next> {
+    this.value = value;
+    if (this.value$) this.value$.next(value);
+    else this.value$ = new BehaviorSubject<Value>(value);
     return this as unknown as LocalStorageItem<Next>;
   }
 
@@ -38,20 +28,26 @@ export class LocalStorageItem<Value> {
    * Load a value from `LocalStorage` and overwrite the current one.
    * @returns
    */
-  load() {
+  load(): this {
+    const VALUE_NOT_EXIST = Symbol();
+    const VALUE_NOT_VALID = Symbol();
     try {
       const raw = localStorage.getItem(this.key);
-      if (raw == null) throw new UseInitialValue();
+      if (raw == null) throw VALUE_NOT_EXIST;
       const dirty = JSON.parse(raw);
-      if (!this.validator(dirty)) throw new UseInitialValue();
+      if (!this.validator(dirty)) throw VALUE_NOT_VALID;
       const validated = dirty;
       this.next(validated);
     } catch (error) {
-      if (!(error instanceof UseInitialValue || error instanceof SyntaxError))
-        throw error;
-      this.next(
-        this.initial instanceof Function ? this.initial() : this.initial,
-      );
+      if (
+        error == VALUE_NOT_EXIST ||
+        error == VALUE_NOT_VALID ||
+        error instanceof SyntaxError
+      ) {
+        const initial =
+          this.initial instanceof Function ? this.initial() : this.initial;
+        this.next(initial);
+      } else throw error;
     } finally {
       return this;
     }
@@ -61,12 +57,8 @@ export class LocalStorageItem<Value> {
    * Save the current value to `LocalStorage`.
    * @returns
    */
-  save() {
+  save(): this {
     localStorage.setItem(this.key, JSON.stringify(this.value));
     return this;
   }
 }
-
-class UseInitialValue {}
-
-type Key = string;
