6c693d3168a27b3f3c9f33ae07a73bb5ed744e7e
Char2s <Char2s@outlook.com>
2022-02-26 14:22:35 +0800

    refactor(auth/layout): merge into the module shell component

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,32 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthFormLoginComponent } from './auth-form-login/auth-form-login.component';
 import { AuthFormSignupComponent } from './auth-form-signup/auth-form-signup.component';
-import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { AuthComponent } from './auth.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: AuthLayoutComponent,
+    component: AuthComponent,
     children: [
       {
         path: '',
-        component: AuthComponent,
-        children: [
-          {
-            path: '',
-            redirectTo: 'login',
-            pathMatch: 'full',
-          },
-          {
-            path: 'login',
-            component: AuthFormLoginComponent,
-          },
-          {
-            path: 'signup',
-            component: AuthFormSignupComponent,
-          },
-        ],
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: AuthFormLoginComponent,
+      },
+      {
+        path: 'signup',
+        component: AuthFormSignupComponent,
       },
     ],
   },
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -5,13 +5,11 @@ import { SharedModule } from '../shared/shared.module';
 import { AuthFormLayoutComponent } from './auth-form-layout/auth-form-layout.component';
 import { AuthFormLoginComponent } from './auth-form-login/auth-form-login.component';
 import { AuthFormSignupComponent } from './auth-form-signup/auth-form-signup.component';
-import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 
 @NgModule({
   declarations: [
-    AuthLayoutComponent,
     AuthComponent,
     AuthFormLayoutComponent,
     AuthFormLoginComponent,
